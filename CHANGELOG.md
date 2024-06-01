cd447bd6345985c6d71259d485099a61d2f58ceb
Rüzgar Engür <ruzgarengur@protonmail.com>
2024-06-01 18:29:27 +0000

    Update 0.3a

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## In the future
+- *SRV,* *TXT,* *DNSKEY,* *PTR,* *CNAME* and *CERT* record support
+- Response beautifying 
+
+## V0.3a
+Added support for *MX,* *NS* and *AAAA* records
+Proxy check for NS and MX records were added to automatically disable it.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from dotenv import load_dotenv
 load_dotenv() 
 import os
@@ -6,7 +7,7 @@ import os
 api_key = os.environ['API_KEY']
 zone_id = os.environ['ZONE_ID']
 url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
-record = input("What type of record so you want to add?")
+record = input("What type of record so you want to add?\n")
 
 headers = {
     "Authorization": f"Bearer {api_key}",
@@ -14,24 +15,33 @@ headers = {
 }
 
 
-def create_dns_record_typeA(headers):
+def create_dns_record_type_generic(headers):
     targetIP = input("'A' record Target\n")
     name = input(f"Name that will point at {targetIP}\n")
     owner_email = input(f"Email information of the Registrar of {name}\n")
+    askproxy = input("Enable CloudFlare proxy? (y/n)\n")
+    if record == "NS" or record == "MX":
+        proxy = False
+    elif askproxy == "y":
+        proxy = True
+    else:
+        proxy = False
+    print(proxy)
     data = {
         "type": f"{record}",
         "name": f"{name}",
         "content": f"{targetIP}",
         "ttl": 3600,
-        "proxied": True,
-        "comment": f"Email: {owner_email}"
+        "proxied": proxy,
+        "comment": f"Email: {owner_email}",
+        "priority": 0
     }
     global response
     response = requests.post(url, json=data, headers=headers)
 
 
-if record == "A":
-    create_dns_record_typeA(headers)
+if record == "A" or record == "MX" or record == "AAAA" or record == "NS":
+    create_dns_record_type_generic(headers)
     print(response.json())
 else:
-    print("Invalid input, only A type records are available for now.")
+    print("Invalid input, only A, AAAA, MX, NS type records are available for now.")
